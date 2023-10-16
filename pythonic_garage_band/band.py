9ed1d5cb3e002e4331d9b6a88f5dd74f058cc4c1
Casey Glidewell <caseyglidewell@gmail.com>
2023-10-16 15:53:06 -0700

    first two bassist tests passing.

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -22,7 +22,17 @@ class Guitarist:
 
 
 class Bassist:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"My name is {self.name} and I play bass"
+
+    def __repr__(self):
+        return f"Bassist instance. Name = {self.name}"
+
     pass
+
 
 
 class Drummer:
